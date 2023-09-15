44ca2d5d2781639c901ad619aa2daaefd9e4abcb
dofu18 <phu2003hk@gmail.com>
2023-09-15 11:17:14 +0700

    lan dau lam chuyen ay

---
+++ src/swp391/Swp391.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swp391;
+
+/**
+ *
+ * @author phu20
+ */
+public class Swp391 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to java github.");
+    }
+    
+}
